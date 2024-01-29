151541faed2f6fd79a57d53fe2816468c1a597aa
Max Greive <maxgreive@googlemail.com>
2024-01-29 16:17:43 +0100

    fix endpoint

--- assets/js/tournaments-map.js
+++ assets/js/tournaments-map.js
@@ -1,11 +1,10 @@
 getTournaments((a) => initMap(a));
 
 async function getTournaments(cb) {
-  // const response = await fetch("https://discgolf-tournaments-api-45d839f9ba85.herokuapp.com/");
   const cachedData = window.sessionStorage.getItem('tournament-data');
 
   if (!cachedData) {
-    const response = await fetch("http://localhost:8080/");
+    const response = await fetch("https://discgolf-tournaments-api-45d839f9ba85.herokuapp.com/");
     const tournaments = await response.json();
     window.sessionStorage.setItem('tournament-data', JSON.stringify(tournaments));
     return cb(tournaments);

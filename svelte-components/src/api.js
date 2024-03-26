dc6419e2f6e6ca6bd28d126c2b4b73ae82cc4510
Max Greive <maxgreive@googlemail.com>
2024-03-26 15:20:33 +0100

    revert

--- svelte-components/src/api.js
+++ svelte-components/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_URL || 'http://127.0.0.1:8080';
+const API_URL = process.env.API_URL || 'https://api.syndikat.golf';
 
 export async function fetchProducts(query, endpoint) {
   const data = [];

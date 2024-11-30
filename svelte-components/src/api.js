818167692397c600d5bf62e509b5d82d2ef60ed2
Max Greive <maxgreive@googlemail.com>
2024-11-30 22:03:19 +0100

    fix URL

--- svelte-components/src/api.js
+++ svelte-components/src/api.js
@@ -1,8 +1,8 @@
 const API_URL = process.env.API_URL || 'https://api.syndikat.golf';
 
-export async function fetchProducts(url, query, endpoint) {
+export async function fetchProducts(query, endpoint) {
   const data = [];
-  const response = await fetch(`${url}/products/${endpoint}/${query}`, {
+  const response = await fetch(`${API_URL}/products/${endpoint}/${query}`, {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
